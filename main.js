339f6dc231b0f4e268dadd71efc9d97c2749d807
Dani Crosby <dani.crosby1111@gmail.com>
2021-01-31 13:23:30 -0600

    form functionality

--- main.js
+++ main.js
@@ -8,21 +8,73 @@ const houses = [
   "Voledemorts Army",
 ];
 
+const printToDom = (divId, textToPrint) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = textToPrint;
+}
+
+const createCard = (obj) => {
+  let domString = "";
+    domString += `
+                <div class="card d-flex mx-auto text-center" style="width: 20em; height: 20em" id=${Math.random}>
+                <h5 class="card-header">STUDENT</h5>
+                <div class="card-body">
+                  <h5 class="card-title">${obj.name}</h5>
+                  <p class="card-text">${obj.house}</p>
+                  <a href="#" class="btn btn-dark">EXPEL</a>
+                </div>
+              </div>`;
+
+    printToDom("#students", domString);
+
+  }
+
+
+
+// const chooseRandomHouse = () => {
+//   const randomNum = Math.floor(Math.random() * colors.length);
+//   document.querySelector('body').style.background = colors[randomNum];
+// };
+
+
 const handleButtonClick = (e) => {
   const buttonId = e.target.id;
 
   if (buttonId === "btn") {
     document.querySelector("#form").innerHTML = `
-                <div class="input-group mb-3 mx-auto">
-              <input type="text" class="form-control" id="name" placeholder="Student's Name..." aria-label="Recipient's username" aria-describedby="button-addon2">
-              <button class="btn btn-outline-secondary" type="button" id="sort">SORT</button>
-            </div>`;
+            <div class="card mx-auto" id="input-form" style="width: 40em">
+              <h5 class="card-header">Enter First Year's Student Name</h5>
+              <div class="card-body d-flex float-left">
+                <input type="text" class="form-control mr-3" style="width: 30em" id="name" placeholder="Student's Name..." aria-label="Recipient's username" aria-describedby="button-addon2">
+                <br>
+                <button class="btn btn-outline-secondary" type="button" style=""id="sort">SORT</button>
+              </div>
+            </div>
+                `;
+
+      document.querySelector("#sort").addEventListener("click", getFormInfo);
   }
 };
 
+const getFormInfo = (e) => {
+  e.preventDefault();
+
+  const name = document.querySelector('#name').value;
+
+  const obj = {
+    name,
+    house: "Gryffindor"
+  }
+
+  createCard(obj);
+  createCard(houses);
+
+  document.querySelector('#name').value = " ";
+
+}
+
 const buttonEvents = () => {
   document.querySelector("#btn").addEventListener("click", handleButtonClick);
-  document.querySelector("#form").addEventListener("click", handleButtonClick);
 };
 
 const init = () => {
