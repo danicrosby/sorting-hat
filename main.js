4e75f4dae0c1a1916d4eee872099316a501ed1aa
Dani Crosby <dani.crosby1111@gmail.com>
2021-02-01 22:26:08 -0600

    need to fix required

--- main.js
+++ main.js
@@ -39,8 +39,7 @@ const handleButtonClick = (e) => {
             <div class="card mx-auto" id="input-form" style="width: 40em">
               <h5 class="card-header m-2">Enter First Year's Student Name</h5>
               <div class="card-body d-flex float-left">
-                <input type="text" required class="form-control w-30 mr-3" id="name" placeholder="Student's Name..." aria-describedby="button-addon2">
-                <br>
+                <input type="text" class="form-control w-30 mr-3" id="name" placeholder="Student's Name..." aria-describedby="button-addon2">
                 <button class="btn btn-outline-secondary" type="submit" id="sort">SORT</button>
               </div>
             </div>`;
@@ -57,12 +56,10 @@ const getFormInfo = (e) => {
   const randomHouseIndex = Math.floor(Math.random() * houses.length);
   const house = houses[randomHouseIndex];
 
-  // get all the student IDs and create a new array and sort them lowest to highest
   const studentIds = students
     .map((student) => student.id)
     .sort((a, b) => a - b);
 
-  // ternary operator: if the students array is not empty, create an ID that is +1 of the last item in the array. Otherwise make the id 1
   const id = studentIds.length ? studentIds[studentIds.length - 1] + 1 : 1;
 
   const obj = {
@@ -82,7 +79,9 @@ const deleteCard = (e) => {
   const targetId = Number(e.target.id);
 
   if (targetType === "button") {
-    const studentIndex = students.findIndex((student) => student.id === targetId);
+    const studentIndex = students.findIndex(
+      (student) => student.id === targetId
+    );
     students.splice(studentIndex, 1);
   }
   createCard(students);
@@ -98,6 +97,3 @@ const init = () => {
 };
 
 init();
-
-    // this method returns the index of the object you are trying to remove in the array (85_)
-    // this removes the item from the array - NOTE: this modifies the original array 86
